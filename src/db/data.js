0666002461a1ad7225310b2f5bd441ff08b4dfd8
Patrice-H <webmestre64@gmail.com>
2022-09-02 02:18:45 +0200

    Adding data

---
+++ src/db/data.js
@@ -0,0 +1,28 @@
+const tasks = [
+  {
+    id: 1,
+    name: 'Faire les courses',
+    category: 'to do',
+    order: 1,
+  },
+  {
+    id: 2,
+    name: 'Préparer le repas',
+    category: 'to do',
+    order: 2,
+  },
+  {
+    id: 3,
+    name: 'Manger',
+    category: 'to do',
+    order: 3,
+  },
+  {
+    id: 4,
+    name: 'Faire la vaisselle',
+    category: 'to do',
+    order: 4,
+  },
+];
+
+module.exports = tasks;
