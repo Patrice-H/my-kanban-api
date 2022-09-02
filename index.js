687d9bf7f618964608c12dbb55d9a678d434042b
Patrice-H <webmestre64@gmail.com>
2022-09-02 03:17:16 +0200

    Adding entrie points

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const readAlltasks = require('./src/routes/readAllTasks');
 const readTaskById = require('./src/routes/readTaskById');
+const createTask = require('./src/routes/createTask');
+const updateTask = require('./src/routes/updateTask');
+const deleteTask = require('./src/routes/deleteTask');
 const { server, port, url } = require('./src/utils/config');
 
 server.get('/', (req, res) => res.send('My Kanban API'));
 readAlltasks(server);
 readTaskById(server);
+createTask(server);
+updateTask(server);
+deleteTask(server);
 
 server.listen(port, () => console.log(`Server listening on : ${url}:${port}`));
