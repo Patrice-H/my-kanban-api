2c93cd31553e415208d66d5ba881dc129ee2bd8c
Patrice-H <webmestre64@gmail.com>
2022-09-02 02:22:02 +0200

    Adding entrie points

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const readAlltasks = require('./src/routes/readAllTasks');
+const readTaskById = require('./src/routes/readTaskById');
 const { server, port, url } = require('./src/utils/config');
 
 server.get('/', (req, res) => res.send('My Kanban API'));
+readAlltasks(server);
+readTaskById(server);
 
 server.listen(port, () => console.log(`Server listening on : ${url}:${port}`));
