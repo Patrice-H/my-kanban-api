3e86eb2509c5135e80fb21f7cff10d5a469d1855
Patrice-H <webmestre64@gmail.com>
2022-09-18 09:15:50 +0200

    Adding API documentation

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const readTaskById = require('./src/routes/readTaskById');
 const createTask = require('./src/routes/createTask');
 const updateTask = require('./src/routes/updateTask');
 const deleteTask = require('./src/routes/deleteTask');
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('yamljs');
+const swaggerDocs = yaml.load('./swagger.yaml');
 
 server
   .use(favicon(__dirname + '/favicon.ico'))
@@ -23,5 +26,7 @@ readTaskById(server);
 createTask(server);
 updateTask(server);
 deleteTask(server);
+
+server.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 server.listen(port, () => console.log(`Server listening on : ${url}:${port}`));
