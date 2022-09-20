5fcf96a6e5b620735eb0325b9c55de0535d574b4
Patrice-H <webmestre64@gmail.com>
2022-09-20 17:32:34 +0200

    Rename categories in french

--- src/db/data.js
+++ src/db/data.js
@@ -2,25 +2,25 @@ const tasks = [
   {
     id: 1,
     title: 'Faire les courses',
-    category: 'to do',
+    category: 'A faire',
     order: 1,
   },
   {
     id: 2,
     title: 'Préparer le repas',
-    category: 'to do',
+    category: 'A faire',
     order: 2,
   },
   {
     id: 3,
     title: 'Manger',
-    category: 'to do',
+    category: 'A faire',
     order: 3,
   },
   {
     id: 4,
     title: 'Faire la vaisselle',
-    category: 'to do',
+    category: 'A faire',
     order: 4,
   },
 ];
