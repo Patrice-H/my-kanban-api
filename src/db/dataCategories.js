9c3e0da780cd4b3cc76c4b64b8f16e4408931742
Patrice-H <webmestre64@gmail.com>
2022-09-22 10:12:16 +0200

    Adding categories data

---
+++ src/db/dataCategories.js
@@ -0,0 +1,22 @@
+const categories = [
+  {
+    id: 1,
+    title: 'A faire',
+    order: 1,
+    dashboard_id: 1,
+  },
+  {
+    id: 2,
+    title: 'En cours',
+    order: 2,
+    dashboard_id: 1,
+  },
+  {
+    id: 3,
+    title: 'Fait',
+    order: 3,
+    dashboard_id: 1,
+  },
+];
+
+module.exports = categories;
