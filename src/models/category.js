db460e85629d24081d90b5d9b404c044e2c350e7
Patrice-H <webmestre64@gmail.com>
2022-09-22 10:11:18 +0200

    Adding a category model

---
+++ src/models/category.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    'Category',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      order: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      dashboard_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+};
