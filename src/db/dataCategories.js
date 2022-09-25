dd1ddb9e9f29e3417d4b535651be05aad4fba686
Patrice-H <webmestre64@gmail.com>
2022-09-25 10:00:18 +0200

    Adding background color data

--- src/db/dataCategories.js
+++ src/db/dataCategories.js
@@ -3,18 +3,21 @@ const categories = [
     id: 1,
     title: 'A faire',
     order: 1,
+    background_color: 'rgba(255, 0, 0, 0.25)',
     dashboard_id: 1,
   },
   {
     id: 2,
     title: 'En cours',
     order: 2,
+    background_color: 'rgba(255, 255, 0, 0.25)',
     dashboard_id: 1,
   },
   {
     id: 3,
     title: 'Fait',
     order: 3,
+    background_color: 'rgba(0, 255, 0, 0.25)',
     dashboard_id: 1,
   },
 ];
