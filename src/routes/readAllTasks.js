741dad06d9bea1e7988d9a693ff6aa53a97bd373
Patrice-H <webmestre64@gmail.com>
2022-10-04 10:55:56 +0200

    Internal server error handling

--- src/routes/readAllTasks.js
+++ src/routes/readAllTasks.js
@@ -2,10 +2,15 @@ const { Task } = require('../db/sequelize');
 
 const readAlltasks = (app) => {
   app.get('/api/tasks', (req, res) => {
-    Task.findAll().then((tasks) => {
-      const message = 'The tasks list has been successfully retrieved';
-      res.json({ message, data: tasks });
-    });
+    Task.findAll()
+      .then((tasks) => {
+        const message = 'The tasks list has been successfully retrieved';
+        res.json({ message, data: tasks });
+      })
+      .catch((error) => {
+        const message = `La liste des tâches n'a pas pu être récupérée ! Veuillez réessayer ultérieurement`;
+        res.status(500).json({ message, data: error });
+      });
   });
 };
 
