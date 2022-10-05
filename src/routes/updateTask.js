bcf2d31833b1c2a6e011189eca5476263c1958cb
Patrice-H <webmestre64@gmail.com>
2022-10-05 10:09:18 +0200

    Internal server error handling on updating tasks

--- src/routes/updateTask.js
+++ src/routes/updateTask.js
@@ -5,18 +5,24 @@ const updateTask = (app) => {
     const id = req.params.id;
     Task.update(req.body, {
       where: { id: id },
-    }).then(() => {
-      Task.findByPk(id).then((updatedTask) => {
-        if (updatedTask === null) {
-          const message =
-            "La tâche demandée n'existe pas. Réessayez avec un autre identifiant";
+    })
+      .then(() => {
+        return Task.findByPk(id).then((updatedTask) => {
+          if (updatedTask === null) {
+            const message =
+              "La tâche demandée n'existe pas. Réessayez avec un autre identifiant";
 
-          return res.status(404).json({ message });
-        }
-        const message = 'La tâche a bien été mise à jour';
-        res.json({ message, data: updatedTask });
+            return res.status(404).json({ message });
+          }
+          const message = 'La tâche a bien été mise à jour';
+          res.json({ message, data: updatedTask });
+        });
+      })
+      .catch((error) => {
+        const message =
+          "La tâche n'a pas pu être mise à jour. Veuillez réessayer ultérieurement";
+        res.status(500).json({ message, data: error });
       });
-    });
   });
 };
 
