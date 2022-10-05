0dea468eaa8b17b1ed6f7c93b468409739e3dd26
Patrice-H <webmestre64@gmail.com>
2022-10-05 09:24:41 +0200

    Internal server error handling

--- src/routes/readTaskById.js
+++ src/routes/readTaskById.js
@@ -2,16 +2,22 @@ const { Task } = require('../db/sequelize');
 
 const readTaskById = (app) => {
   app.get('/api/task/:id', (req, res) => {
-    Task.findByPk(req.params.id).then((task) => {
-      if (task === null) {
-        const message =
-          "La tâche demandée n'existe pas. Réessayez avec un autre identifiant";
+    Task.findByPk(req.params.id)
+      .then((task) => {
+        if (task === null) {
+          const message =
+            "La tâche demandée n'existe pas. Réessayez avec un autre identifiant";
 
-        return res.status(404).json({ message });
-      }
-      const message = 'La tâche a bien été récupérée';
-      res.json({ message, data: task });
-    });
+          return res.status(404).json({ message });
+        }
+        const message = 'La tâche a bien été récupérée';
+        res.json({ message, data: task });
+      })
+      .catch((error) => {
+        const message =
+          "La tâche n'a pas pu être récupérée. Veuillez réessayer ultérieurement";
+        res.status(500).json({ message, data: error });
+      });
   });
 };
 
