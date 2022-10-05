94573630d7796bab0b88efe97401952a4fbd63ce
Patrice-H <webmestre64@gmail.com>
2022-10-05 09:57:58 +0200

    Internal server error handling on deleting tasks

--- src/routes/deleteTask.js
+++ src/routes/deleteTask.js
@@ -2,21 +2,28 @@ const { Task } = require('../db/sequelize');
 
 const deleteTask = (app) => {
   app.delete('/api/task/:id', (req, res) => {
-    Task.findByPk(req.params.id).then((task) => {
-      const deletedTask = task;
-      if (task === null) {
-        const message =
-          "La tâche demandée n'existe pas. Réessayez avec un autre identifiant";
+    Task.findByPk(req.params.id)
+      .then((task) => {
+        const deletedTask = task;
+        if (task === null) {
+          const message =
+            "La tâche demandée n'existe pas. Réessayez avec un autre identifiant";
 
-        return res.status(404).json({ message });
-      }
-      Task.destroy({
-        where: { id: task.id },
-      }).then(() => {
-        const message = 'La tâche a bien été supprimée';
-        res.json({ message, data: deletedTask });
+          return res.status(404).json({ message });
+        }
+
+        return Task.destroy({
+          where: { id: task.id },
+        }).then(() => {
+          const message = 'La tâche a bien été supprimée';
+          res.json({ message, data: deletedTask });
+        });
+      })
+      .catch((error) => {
+        const message =
+          "La tâche n'a pas pu être supprimée. Veuillez réessayer ultérieurement";
+        res.status(500).json({ message, data: error });
       });
-    });
   });
 };
 
