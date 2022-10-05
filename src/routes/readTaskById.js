5b658e463f1ccf4f0529eb568cd458b544fa9d55
Patrice-H <webmestre64@gmail.com>
2022-10-05 09:13:59 +0200

    Handing 404 errors on tasks

--- src/routes/readTaskById.js
+++ src/routes/readTaskById.js
@@ -3,6 +3,12 @@ const { Task } = require('../db/sequelize');
 const readTaskById = (app) => {
   app.get('/api/task/:id', (req, res) => {
     Task.findByPk(req.params.id).then((task) => {
+      if (task === null) {
+        const message =
+          "La tâche demandée n'existe pas. Réessayez avec un autre identifiant";
+
+        return res.status(404).json({ message });
+      }
       const message = 'The task has been found';
       res.json({ message, data: task });
     });
