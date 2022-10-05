081c41b057e18c272d5da3c297a9e4339edc0bd8
Patrice-H <webmestre64@gmail.com>
2022-10-05 10:04:09 +0200

    Handling 404 errors on updating tasks

--- src/routes/updateTask.js
+++ src/routes/updateTask.js
@@ -7,6 +7,12 @@ const updateTask = (app) => {
       where: { id: id },
     }).then(() => {
       Task.findByPk(id).then((updatedTask) => {
+        if (updatedTask === null) {
+          const message =
+            "La tâche demandée n'existe pas. Réessayez avec un autre identifiant";
+
+          return res.status(404).json({ message });
+        }
         const message = 'La tâche a bien été mise à jour';
         res.json({ message, data: updatedTask });
       });
