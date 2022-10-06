dc67abb544ef6dae6ace762dc0a35dd3660b6ddf
Patrice-H <webmestre64@gmail.com>
2022-10-06 17:39:47 +0200

    Internal server error handling on creating tasks

--- src/routes/createTask.js
+++ src/routes/createTask.js
@@ -2,10 +2,15 @@ const { Task } = require('../db/sequelize');
 
 const createTask = (app) => {
   app.post('/api/tasks', (req, res) => {
-    Task.create(req.body).then((newTask) => {
-      const message = 'La tâche a bien créée et ajoutée à la liste';
-      res.json({ message, data: newTask });
-    });
+    Task.create(req.body)
+      .then((newTask) => {
+        const message = 'La tâche a bien créée et ajoutée à la liste';
+        res.json({ message, data: newTask });
+      })
+      .catch((error) => {
+        const message = `La tâche n'a pas pu être créée ! Veuillez réessayer ultérieurement`;
+        res.status(500).json({ message, data: error });
+      });
   });
 };
 
