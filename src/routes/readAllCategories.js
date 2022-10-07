f5655e1ba283260b405597738c3bada66ea5449e
Patrice-H <webmestre64@gmail.com>
2022-10-07 08:45:26 +0200

    Internal server error handling on getting categories list

--- src/routes/readAllCategories.js
+++ src/routes/readAllCategories.js
@@ -2,10 +2,15 @@ const { Category } = require('../db/sequelize');
 
 const readAllCategories = (app) => {
   app.get('/api/categories', (req, res) => {
-    Category.findAll().then((categories) => {
-      const message = 'La liste des catégories a bien été récupérée';
-      res.json({ message, data: categories });
-    });
+    Category.findAll()
+      .then((categories) => {
+        const message = 'La liste des catégories a bien été récupérée';
+        res.json({ message, data: categories });
+      })
+      .catch((error) => {
+        const message = `La liste des catégories n'a pas pu être récupérée ! Veuillez réessayer ultérieurement`;
+        res.status(500).json({ message, data: error });
+      });
   });
 };
 
