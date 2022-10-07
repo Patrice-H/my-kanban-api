62480a5ec33e124c7d12db46a7b25d91c0f6020e
Patrice-H <webmestre64@gmail.com>
2022-10-07 08:59:55 +0200

    Internal server error handling on updating categories

--- src/routes/updateCategory.js
+++ src/routes/updateCategory.js
@@ -5,18 +5,24 @@ const updateCategory = (app) => {
     const id = req.params.id;
     Category.update(req.body, {
       where: { id: id },
-    }).then(() => {
-      Category.findByPk(id).then((updatedCategory) => {
-        if (updatedTask === null) {
-          const message =
-            "La catégorie demandée n'existe pas. Réessayez avec un autre identifiant";
+    })
+      .then(() => {
+        return Category.findByPk(id).then((updatedCategory) => {
+          if (updatedCategory === null) {
+            const message =
+              "La catégorie demandée n'existe pas. Réessayez avec un autre identifiant";
 
-          return res.status(404).json({ message });
-        }
-        const message = 'La catégorie a bien été mise à jour';
-        res.json({ message, data: updatedCategory });
+            return res.status(404).json({ message });
+          }
+          const message = 'La catégorie a bien été mise à jour';
+          res.json({ message, data: updatedCategory });
+        });
+      })
+      .catch((error) => {
+        const message =
+          "La catégorie n'a pas pu être mise à jour. Veuillez réessayer ultérieurement";
+        res.status(500).json({ message, data: error });
       });
-    });
   });
 };
 
