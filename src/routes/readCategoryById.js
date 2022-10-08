432399f138df4092c3f0788d394665cfeacd62fc
Patrice-H <webmestre64@gmail.com>
2022-10-08 09:17:23 +0200

    Internal server error handling on getting category

--- src/routes/readCategoryById.js
+++ src/routes/readCategoryById.js
@@ -2,16 +2,22 @@ const { Category } = require('../db/sequelize');
 
 const readCategoryById = (app) => {
   app.get('/api/category/:id', (req, res) => {
-    Category.findByPk(req.params.id).then((category) => {
-      if (category === null) {
-        const message =
-          "La catégorie demandée n'existe pas. Réessayez avec un autre identifiant";
+    Category.findByPk(req.params.id)
+      .then((category) => {
+        if (category === null) {
+          const message =
+            "La catégorie demandée n'existe pas. Réessayez avec un autre identifiant";
 
-        return res.status(404).json({ message });
-      }
-      const message = 'La catégorie a bien été récupérée';
-      res.json({ message, data: category });
-    });
+          return res.status(404).json({ message });
+        }
+        const message = 'La catégorie a bien été récupérée';
+        res.json({ message, data: category });
+      })
+      .catch((error) => {
+        const message =
+          "La catégorie n'a pas pu être récupérée. Veuillez réessayer ultérieurement";
+        res.status(500).json({ message, data: error });
+      });
   });
 };
 
