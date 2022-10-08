d79f35fc74761d6938abc1966ff1ef946c63e128
Patrice-H <webmestre64@gmail.com>
2022-10-08 09:40:13 +0200

    Display of the success message in French

--- src/routes/readAllDashboards.js
+++ src/routes/readAllDashboards.js
@@ -3,7 +3,7 @@ const { Dashboard } = require('../db/sequelize');
 const readAllDashboards = (app) => {
   app.get('/api/dashboards', (req, res) => {
     Dashboard.findAll().then((dashboards) => {
-      const message = 'The dashboards list has been retrieved';
+      const message = 'La liste des tableaux de bord a bien été récupérée';
       res.json({ message, data: dashboards });
     });
   });
