b9b6d81df86c6063b4ea9f93cd12f48c717ade8f
Patrice-H <webmestre64@gmail.com>
2022-10-08 09:32:42 +0200

    Internal server error handling on deleting category

--- src/routes/deleteCategory.js
+++ src/routes/deleteCategory.js
@@ -2,21 +2,28 @@ const { Category } = require('../db/sequelize');
 
 const deleteCategory = (app) => {
   app.delete('/api/category/:id', (req, res) => {
-    Category.findByPk(req.params.id).then((category) => {
-      const deletedCategory = category;
-      if (category === null) {
-        const message =
-          "La catégorie demandée n'existe pas. Réessayez avec un autre identifiant";
+    Category.findByPk(req.params.id)
+      .then((category) => {
+        const deletedCategory = category;
+        if (category === null) {
+          const message =
+            "La catégorie demandée n'existe pas. Réessayez avec un autre identifiant";
 
-        return res.status(404).json({ message });
-      }
-      Category.destroy({
-        where: { id: category.id },
-      }).then(() => {
-        const message = 'La catégorie a bien été supprimée';
-        res.json({ message, data: deletedCategory });
+          return res.status(404).json({ message });
+        }
+
+        return Category.destroy({
+          where: { id: category.id },
+        }).then(() => {
+          const message = 'La catégorie a bien été supprimée';
+          res.json({ message, data: deletedCategory });
+        });
+      })
+      .catch((error) => {
+        const message =
+          "La catégorie n'a pas pu être supprimée. Veuillez réessayer ultérieurement";
+        res.status(500).json({ message, data: error });
       });
-    });
   });
 };
 
