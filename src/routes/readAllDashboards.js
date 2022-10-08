bbc80d36fc583491e4f64e69e84105b6a3b81cf4
Patrice-H <webmestre64@gmail.com>
2022-10-08 09:42:47 +0200

    Internal server error handling on getting dashboards list

--- src/routes/readAllDashboards.js
+++ src/routes/readAllDashboards.js
@@ -2,10 +2,15 @@ const { Dashboard } = require('../db/sequelize');
 
 const readAllDashboards = (app) => {
   app.get('/api/dashboards', (req, res) => {
-    Dashboard.findAll().then((dashboards) => {
-      const message = 'La liste des tableaux de bord a bien été récupérée';
-      res.json({ message, data: dashboards });
-    });
+    Dashboard.findAll()
+      .then((dashboards) => {
+        const message = 'La liste des tableaux de bord a bien été récupérée';
+        res.json({ message, data: dashboards });
+      })
+      .catch((error) => {
+        const message = `La liste des tableaux de bord n'a pas pu être récupérée ! Veuillez réessayer ultérieurement`;
+        res.status(500).json({ message, data: error });
+      });
   });
 };
 
