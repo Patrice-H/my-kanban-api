f4e0ecf472bc63c18e02f231dfa2ceeabd06e7f9
Patrice-H <webmestre64@gmail.com>
2022-10-08 09:37:18 +0200

    Internal server error handling on creating category

--- src/routes/createCategory.js
+++ src/routes/createCategory.js
@@ -2,10 +2,15 @@ const { Category } = require('../db/sequelize');
 
 const createCategory = (app) => {
   app.post('/api/categories', (req, res) => {
-    Category.create(req.body).then((newCategory) => {
-      const message = 'La catégorie a bien créée et ajoutée à la liste';
-      res.json({ message, data: newCategory });
-    });
+    Category.create(req.body)
+      .then((newCategory) => {
+        const message = 'La catégorie a bien créée et ajoutée à la liste';
+        res.json({ message, data: newCategory });
+      })
+      .catch((error) => {
+        const message = `La catégorie n'a pas pu être créée ! Veuillez réessayer ultérieurement`;
+        res.status(500).json({ message, data: error });
+      });
   });
 };
 
