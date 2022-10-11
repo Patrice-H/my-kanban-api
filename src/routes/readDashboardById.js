377747bb46ce15ca6ef67b91d462b3cf439312d9
Patrice-H <webmestre64@gmail.com>
2022-10-11 18:47:53 +0200

    Display of the success message in French

--- src/routes/readDashboardById.js
+++ src/routes/readDashboardById.js
@@ -3,7 +3,7 @@ const { Dashboard } = require('../db/sequelize');
 const readDashboardById = (app) => {
   app.get('/api/dashboard/:id', (req, res) => {
     Dashboard.findByPk(req.params.id).then((dashboard) => {
-      const message = 'The dashboard has been found';
+      const message = 'Le tableau de bord a bien été récupéré';
       res.json({ message, data: dashboard });
     });
   });
