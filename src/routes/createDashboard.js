38eae8d9cbf618c8cdcda3774ebc2ded9e79b52c
Patrice-H <webmestre64@gmail.com>
2022-10-11 18:45:41 +0200

    Internal server error handling on creating dashboard

--- src/routes/createDashboard.js
+++ src/routes/createDashboard.js
@@ -2,11 +2,16 @@ const { Dashboard } = require('../db/sequelize');
 
 const createDashboard = (app) => {
   app.post('/api/dashboards', (req, res) => {
-    Dashboard.create(req.body).then((newDashboard) => {
-      const message =
-        'Le tableau de bord a bien été créé et ajoutée à la liste';
-      res.json({ message, data: newDashboard });
-    });
+    Dashboard.create(req.body)
+      .then((newDashboard) => {
+        const message =
+          'Le tableau de bord a bien été créé et ajouté à la liste';
+        res.json({ message, data: newDashboard });
+      })
+      .catch((error) => {
+        const message = `Le tableau de bord n'a pas pu être créé ! Veuillez réessayer ultérieurement`;
+        res.status(500).json({ message, data: error });
+      });
   });
 };
 
