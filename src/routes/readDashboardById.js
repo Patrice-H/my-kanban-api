0633460d8e5a65e4f9c8f04e5b2a09d54e1945e2
Patrice-H <webmestre64@gmail.com>
2022-10-11 18:52:17 +0200

    Internal server error handling on getting dashboard

--- src/routes/readDashboardById.js
+++ src/routes/readDashboardById.js
@@ -2,16 +2,22 @@ const { Dashboard } = require('../db/sequelize');
 
 const readDashboardById = (app) => {
   app.get('/api/dashboard/:id', (req, res) => {
-    Dashboard.findByPk(req.params.id).then((dashboard) => {
-      if (dashboard === null) {
-        const message =
-          "Le tableau de bord demandé n'existe pas. Réessayez avec un autre identifiant";
+    Dashboard.findByPk(req.params.id)
+      .then((dashboard) => {
+        if (dashboard === null) {
+          const message =
+            "Le tableau de bord demandé n'existe pas. Réessayez avec un autre identifiant";
 
-        return res.status(404).json({ message });
-      }
-      const message = 'Le tableau de bord a bien été récupéré';
-      res.json({ message, data: dashboard });
-    });
+          return res.status(404).json({ message });
+        }
+        const message = 'Le tableau de bord a bien été récupéré';
+        res.json({ message, data: dashboard });
+      })
+      .catch((error) => {
+        const message =
+          "Le tableau de bord n'a pas pu être récupéré. Veuillez réessayer ultérieurement";
+        res.status(500).json({ message, data: error });
+      });
   });
 };
 
