5db3e203e7a7a09025a06d9733dbd5bf816b72c1
Patrice-H <webmestre64@gmail.com>
2022-10-11 18:50:34 +0200

    Handling 404 errors on getting dashboard

--- src/routes/readDashboardById.js
+++ src/routes/readDashboardById.js
@@ -3,6 +3,12 @@ const { Dashboard } = require('../db/sequelize');
 const readDashboardById = (app) => {
   app.get('/api/dashboard/:id', (req, res) => {
     Dashboard.findByPk(req.params.id).then((dashboard) => {
+      if (dashboard === null) {
+        const message =
+          "Le tableau de bord demandé n'existe pas. Réessayez avec un autre identifiant";
+
+        return res.status(404).json({ message });
+      }
       const message = 'Le tableau de bord a bien été récupéré';
       res.json({ message, data: dashboard });
     });
